e1e89c2f91b0cc39a84cf402f915e5e513e259ff
User0 <0@0>
2021-12-18 18:12:56 +1000

    Commit 2 diff branch 2.2

--- app/src/main/java/com/example/usinggit/MainActivity.java
+++ app/src/main/java/com/example/usinggit/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
         String hellasafao = "Hello Git!";
 
-        //commit 2 diff branch
+        //commit 2 diff branch 2222222
 
     }
 }
